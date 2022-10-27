b306b7ab49be2247223c718960b3822d639bd968
bbayong28 <ghkdud5795@hanmail.net>
2022-10-27 17:46:38 +0900

    20221027

--- nmd_react/src/App.js
+++ nmd_react/src/App.js
@@ -3,6 +3,16 @@ import React, { useEffect, useState } from "react"
 function App() { 
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (e) => {
+    setCost(e.target.value);
+    setNeed(1);
+  }
+  const handleInput = (event) => {
+    setNeed(event.target.value);
+  } 
+
   useEffect(() => {
     //개발자도구 열어서 Network가면 tickers 있음. 많은 코인 정보를 받았음.
     //response로 부터 json을 추출해내고 싶음  .then을 써줌
@@ -17,21 +27,37 @@ function App() {
   
   return (
     <div>
-      <h1>The Conins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading....</strong>
-      ) : (
-      <select>
-          {coins.map((coin) => (
-              <option>
-                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-        </select>
-      )}
-      
+    <h1>The coins!{loading ? "" :`Here are..${coins.length} coins`}</h1>
+    {loading ? <strong>loading...</strong> : <select onChange={onChange}>
+      <option>Select Coin!</option>
+      {coins.map((coin , index) =>
+      <option 
+        key={index} 
+        value={coin.quotes.USD.price} 
+        id={coin.symbol}
+        symbol = {coin.symbol} >
+        {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+        </option>
+       )}
+    </select>}
+    <h2>Please enter the amount</h2>
+    <div>
+      <input type="number" value={need} onChange={handleInput} placeholder="dollor"/>$
     </div>
-  )
-} 
-
+    <h2>You can get {need/cost}</h2>
+  </div>);
+}
 export default App;
+
+
+/*
+const conTit
+
+ (
+    1: <dfdf/>
+    2: <fffff/>
+    3: <ee/>
+    4: <rr/>
+)[conTit] 
+
+*/
